57abac4dffbbee29d15379f84d6a6f79d31e50f5
Hassan Ali <94173090+HassanAli361@users.noreply.github.com>
2022-09-03 13:54:57 +0500

    initialCommit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <router-view />
+</template>
+<script>
+import { defineComponent } from "vue";
+import "aos/dist/aos.css";
+// import '.assets/jsfile'
+
+export default defineComponent({
+  name: "App",
+});
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,54 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-black text-white">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar class="q-ml-sm" style="width: 200px" size="50px">
+            <img src="~assets/logo.png" />
+          </q-avatar>
+        </q-toolbar-title>
+        <q-toolbar-title>
+          <q-tabs active-color="warning" no-caps class="absolute-center">
+            <q-route-tab exact replace label="Home" to="/" />
+          </q-tabs>
+        </q-toolbar-title>
+
+
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+
+<script>
+import { onMounted } from "@vue/runtime-core";
+import { useQuasar } from 'quasar';
+import { ref } from 'vue';
+import AOS from "aos";
+export default {
+  props: {
+    msg: String,
+  },
+  setup() {
+     const $q = useQuasar();
+    onMounted(() => {
+      AOS.init();
+    });
+    return {
+      toggleDarkMode() {
+        $q.dark.toggle();
+      },
+      first: ref(false),
+    };
+  },
+};
+</script>
+
+<style>
+body.body--dark {
+  background: #333333;
+}
+</style>
